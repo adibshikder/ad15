8ffea10c746102992fd9145e3c3e52668f7edced
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:45:06 +0600

    Add files via upload

---
+++ service.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li><li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+					
+				</ul>
+			</div>
+		</nav>
+			<body>
+		<img src="img/Hospital-beds-application.jpg" width="400" height="380"
+		align="left"/>
+		</br>
+		</br>
+		<h1 style="color:LightCoral;">250 Bed</h1><p>
+					The MM Hospital Pvt. Ltd. is a hospital designed to provide </br>
+					quality health care services to the people of Bangladesh in particular,</br> and people of the division and beyond in general.
+					Presently it is a 250 Bed hospital, with an expansion plan to increase it to 500 beds..
+					</p>
+		</br>
+		</br>
+		<img src="img/emergency-services.jpg" width="400" height="300"
+		align="right"/>
+		</br>
+		</br>
+		<h1 style="color:LightCoral;">24/7 SERVICES</h1><p>
+					The Hospital provides round-the-clock emergency and hospitalization services, </br>
+					with a facility that is fully equipped and managed by a dedicated team </br> of qualified emergency medical care professionals.
+					
+		</br>
+	
+	</body>
+			<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
