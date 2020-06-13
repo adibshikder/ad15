fb2a871485b2e1b34961f83835dcb4d6fb2ddd2e
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:40:13 +0600

    Add files via upload

---
+++ goalAndObjectives.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="maincontent">
+			<div class="imgage">
+				<img src="img/goalAndObjectivesPic.jpg" alt="GoalAndObjectivesPic.jpg" width="100%" height="100%" />
+			</div>
+			<div class="whoWeAre">
+				<h1>WHO WE ARE</h1>
+				<p>XYZ Diagnostic Centre Ltd. is an advanced Centre for diagnostic and medical services. It is one of the prestigious diagnostic complexes of Bangladesh which started its activities in 1983.XYZ Diagnostic Centre Ltd. is the largest diagnostic services provider organization in private sector of the country. It is been pioneer in introducing world latest medical equipments and advanced technology to provide round the clock medical investigations and consultancy services.</p>
+			</div>
+			<div class="Details">
+				<div class="goalObjectivesImg">
+					<img src="img/obj.jpg" alt="" width="100%" height="100%"/>
+				</div>
+				<div class="goalObjectives">
+					 <h1>Goal</h1>
+					 <ol>
+					 	<li>To establish a referral Diagnostic and Medical Services Centre.</li>
+					 </ol>
+					 <h1>Objectives</h1>
+					 <ol>
+					 	<li>To render the world standard diagnostic service to the people of the country at an affordable cost and in turn to limit the outflow of the patient abroad at the expense of heard earn foreign currency by providing quality diagnostic services.</li>
+					 	<li>Out door basis treatment by renowned General Practitioners, Consultants and Professors in different medical fields.</li>
+					 	<li>To promote Health Education & Medical Services.</li>
+					 	<li>Day care Centre for follow-up cardiac renal and oncology patients.</li>
+					 	<li>To build a full fledged specialized (Tertiary) Hospital.</li>
+					 </ol>
+				</div>
+				
+			</div>
+		</div>
+		<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
