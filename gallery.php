52e5fb2e5d06e95f23c257a046cb1e935f742b03
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:39:47 +0600

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+					<li><a href="login.php">Login</a></li>
+				</ul>
+			</div>
+		</nav>
+			<!div class="imgage">
+			<body>
+				<img src="img/goalAndObjectivesPic.jpg" alt="GoalAndObjectivesPic.jpg" width="45%" height="45%" />
+				<img src="img/aamc-news-promo-regular-harvard-study-01.jpg__720x480_q85_crop_subsampling-2_upscale.jpg" width="45%" height="45%">
+		
+				<img src="img/doctors_hospital_corridor_nurse_pushing_gurney_stretcher_bed.jpg" width="45%" height="45%">
+		
+				<img src="img/thailand-cave-hospital-left-gty-ps-180711_hpMain_16x9_992.jpg" width="45%" height="45%">
+				<img src="img/doctors-hospital-design-articleLarge.jpg" width="45%" height="45%">
+		
+				<img src="img/download.jpg" width="45%" height="45%">
+			<!/div>
+			</body>
+			
+					<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
