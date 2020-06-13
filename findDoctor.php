fdd960b595cc848b15253e70c5ea87c3ff819858
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:38:25 +0600

    Add files via upload

---
+++ findDoctor.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+					<li><a href="login.php">Login</a></li>
+				</li>
+			</ul>
+				</ul>
+			</div>
+		</nav>
+		<div class="tableContent">
+			<table class="table">
+				<caption>Find Doctor's</caption>
+			    <tr>
+					<th>Doctors Name</th>
+					<th>Department Name</th>
+					<th>Degree</th>
+					<th>Hospital</th>
+					<th>From</th>
+					<th>To</th>
+			    </tr>
+			    <tr>
+					<td>Dr. Georgina</td>
+					<td>Gynaecology</td>
+					<td>MBBS, VVD,MCPS</td>
+					<td>Dhaka Medical College & Hospitaltd</td>
+					<td>10.00 AM</td>
+					<td>2.00 PM</td>
+			    </tr>
+                <tr>
+					<td>Dr. Ronaldo</td>
+					<td>Cardiology</td>
+					<td>MBBS(Dhaka),MD(Cardiology)</td>
+					<td>Dhaka Medical College & Hospital</td>
+					<td>11.00 AM</td>
+					<td>7.00 PM</td>
+			    </tr>
+				<tr>
+					<td>Dr. Mbappe</td>
+					<td>Gynaecology</td>
+					<td>MBBS, FCPS, CMU(DU)</td>
+					<td>Sylhet MAG Osmani Medical College</td>
+					<td>10.00 AM</td>
+					<td>9.00 PM</td>
+			    </tr>
+				<tr>
+					<td>Dr. Cristiano</td>
+					<td>Chest Medicine</td>
+					<td>MBBS, VVD,MCPS</td>
+					<td>Bangabandhu Sheikh Mujib Medical University</td>
+					<td>2.00 PM</td>
+					<td>10.00 PM</td>
+			    </tr>
+				<tr>
+					<td>Dr. Cristianinho</td>
+					<td>Gynaecology</td>
+					<td>MBBS,FCPS</td>
+					<td>Sylhet MAG Osmani Medical College</td>
+					<td>3.00 PM</td>
+					<td>10.00 PM</td>
+			    </tr>
+				<tr>
+					<td>Dr. XYZ</td>
+					<td>Cardiology</td>
+					<td>MBBS,CMU(DU)</td>
+					<td>Bangabandhu Sheikh Mujib Medical University</td>
+					<td>5.00 PM</td>
+					<td>10.00 PM</td>
+			    </tr>
+				<tr>
+					<td>Dr. Neymar</td>
+					<td>Neuro Medicine</td>
+					<td>MBBS,MD(Nurology)</td>
+					<td>Sylhet MAG Osmani Medical College</td>
+					<td>3.30 PM</td>
+					<td>10.00 PM</td>
+			    </tr>
+				<tr>
+					<td>Dr. Marcelo</td>
+					<td>Chest Medicine</td>
+					<td>MBBS,CMU(DU)</td>
+					<td>Sylhet MAG Osmani Medical College</td>
+					<td>11.00 AM</td>
+					<td>4.00 pM</td>
+			    </tr>
+				<tr>
+					<td>Dr. Ramos</td>
+					<td>Rheumatology</td>
+					<td>MBBS,FCPS</td>
+					<td>Dhaka Medical College & Hospital</td>
+					<td>11.30 AM</td>
+					<td>121.00 pM</td>
+			    </tr>
+			  </tr>
+			</table>
+		<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
