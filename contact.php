90d91fa6da6988a57d57ed6c82f36ece367230ea
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:37:11 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+					<li><a href="login.php">Login</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="contact">
+			<p><strong>Address: </strong><i>ZallarPar Road,Hafiz Complex,Zindhabaz,Sylhet</i></p>
+			<hr />
+			<p><strong>Phone: </strong><i>01641........</i></p>
+			<hr />
+			<p><strong>Fax: </strong><i>......</i></p>
+			<hr />
+			<p><strong>Email: </strong><i>adibshikder@gmail.com</i></p>
+			<hr />
+			<p><strong>Customer care: </strong><i>01781.......</i></p>
+			<hr />
+			<p><strong>Web Site: </strong><i>www.adxyzhospitasyl.com</i></p>
+			<hr>
+
+		</div>
+		<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
