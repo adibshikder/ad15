4e4430f233719fcb32e781b56e7322f72c175ce6
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:43:18 +0600

    Add files via upload

---
+++ massageFrom ManagingDirector.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+					<li><a href="login.php">Login</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class=".messageChairman">
+			<div class="chairmanImg">
+				<img src="img/sakib.jpg" alt="Pic" width="100%" height="60%" />
+				<h2>Sakib Al Hasan</h2>
+				<h4>Managing Director,XYZ Hospital</h4>
+			</div>
+			<div class="message">
+				<div class="messageHeader">
+					<h2>Message From Managing Director</h2>
+					<p>XYZ started its journey as diagnostic centre in June 1983 with a Promise to render the possible standard service to the people of the country at an affordable cost and in turn to limit the outflow of the patient abroad at the expense of heard earn foreign currency. When I started working in pathology department of Dhaka Medical College Hospital, I saw that thousands of people of our country are going to abroad every year only for treatment. The country was losing millions of taka worth foreign currency. That time I felt, as a Doctor I must do something about it. I had the spirit but didn't have the resources. From that inspiration I started Popular Diagnostic.
+
+					Bangladesh is a developing country. We have tremendous scope and potentiality in our industry especially in health sector. Basically my father was a Doctor. None of my family members were engaged in business. My dream was to be a surgeon. But necessity and circumstances force me to start diagnostic business. Challenges and problems are many. But solving strategies are very few and precise. As a businessman we should relentlessly concern about our commitment and quality.
+
+					To achieve the goal and ensure the quality service need highly skilled manpower who will continue to produce high level productivity relentlessly maintaining high standard of medical services. I always higher the skilled manpower and motivate them. Being an entrepreneur of a health services provider organization I always concern to ensure the right man in the right place. I believe that if a person whether he is a doctor or not works hard, has merit, good behaviour and remains honest, he will be a successful businessman. Success is nothing but a goal of achievement. In an every success has positive productivity, which is dedicated for the welfare of mankind. Almighty God, Honorable Doctors and my beloved colleagues are always inspired me to do good and take challenges. My all afford to happy them..</p>
+				</div>
+			</div>
+		</div> 	
+		<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
