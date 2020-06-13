f7aa2327c4cb93096455cd228868722426b9d826
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:37:50 +0600

    Add files via upload

---
+++ diagnostic.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+					<li><a href="login.php">Login</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="diagonistic">
+			<div class="availableTest">	
+				<h2>Avaiable Test List</h2>
+				<ol>
+					<li><a href="#">	&#9758; Consulting Room Tests</a></li>
+					<li><a href="#">	&#9758; Cardiovascular</a></li>
+					<li><a href="#">	&#9758; Dermatology</a></li>
+					<li><a href="#">	&#9758; Ear, Nose and Throat</a></li>
+					<li><a href="#">	&#9758; Gastrointestinal</a></li>
+					<li><a href="#">	&#9758; Hematology</a></li>
+					<li><a href="#">	&#9758; Neurological</a></li>
+					<li><a href="#">	&#9758; Ocular</a></li>
+					<li><a href="#">	&#9758; Pulmonary</a></li>
+					<li><a href="#">	&#9758; Rheumatologic</a></li>
+					<li><a href="#">&#9758;	Urologic</a></li>
+				</ol>
+			</div>
+		</div>
+		<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
