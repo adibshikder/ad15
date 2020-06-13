9a9d9d0097d762418cda449ccbf20a259710a6d1
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:32:31 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="login.php">Login</a></li>
+				</ul>
+			</div>
+		</nav>
+			<body>
+		<img src="img/Bendigo1.jpg" width="480" height="500"
+		align="right"/>
+		</br>
+		</br>
+		<h1> <style="color:LightCoral;">More about us</h1>
+		XYZ hospital is a 250-bed multi-disciplinary hospital in Sylhet, Bangladesh, that confidently and capably provides comprehensive health care with the latest medical, surgical and diagnostic equipment and facilities. These services are provided by expert medical professionals, skilled nurses and technologists using state-of- the-art technology.
+
+		XYZ Hospital is an eleven storey building built on one acre of land, and has 120,000 square feet of total floor space. The Hospital is a sister concern of the world renowned perfume brand XYZ. XYZ Hospital has all the characteristics of a world-class hospital with a wide range of services and a diversity of specialists, equipment and technology. The Hospital showcases a synergy of medical technology and advances in IT through paperless medical records. The skilled nurses, technologists and administrators of XYZ Hospital, aided by state-of- the-art equipment, provide a congenial infrastructure for the medical professionals in providing healthcare to international standards.
+		</br>
+		</br>
+		VISION:
+		</br>
+		The vision of XYZ Hospital is to lead the evolution of healthcare and be the most trusted health service provider in Bangladesh.
+		</br>
+		</br>
+		MISSION:
+		</br>
+		The mission of XYZ Hospital is to provide quality health services and facilities for the community, to promote wellness, to relieve suffering and to restore health as swiftly, safely, and humanely as possible, consistent with the best service we can give and at the highest value.
+
+		
+		</br>
+	</body>
+			<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
