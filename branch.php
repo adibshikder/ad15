fb2888e084f86ed9522220bab9cd9f71e5b2ba1d
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:36:42 +0600

    Add files via upload

---
+++ branch.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+					
+				</ul>
+			</div>
+		</nav>
+			<body>
+		<img src="img/dhanmondi.jpg" width="450" height="350"
+		align="left"/>
+		</br>
+		</br>
+		<h1> <style="color:LightCoral;">Dhanmondi Branch(0010)</h1>
+		Address:</br>
+		Quality Center, 744, Shat Masjid Road, Dhanmondi, Dhaka</br>
+		Phone:4811*****,5815*****, 9136*****, 8143***,01716***** (Manager)</br>
+		Email: bm_dhanmondibranch@hospital.com.bd</br>
+		Fax: 02-9139***</br>
+		SWIFT Code: XYZBDDHDMB</br>
+		24hours Service: Yes</br>
+		AD Branch: Yes</br>
+		another Branch: No</br>
+		Opening Date: 12/28/1986</br>
+		
+		</br>
+	</body>
+			<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
