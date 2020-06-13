f529738077d9c747763ac22994f09b9b0deb65ea
adibshikder <60639973+adibshikder@users.noreply.github.com>
2020-06-13 18:44:20 +0600

    Add files via upload

---
+++ messageFromChairman.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="header">
+				<div class="headerLogo">
+					<img src="img/logo.jpg" alt="Logo_Image" width="100%" height="100%"/>
+				</div>
+				<div class="title">
+					<h2>XYZ Hospital</h2>
+				</div>
+			</div>
+		</header>
+		<div class="marquee">
+			<marquee>Welcome to XYZ Hospital........The hospital is an institution in which sick and injured persons are given medical or surgical treatment.</marquee>
+		</div>
+		<nav>
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a>
+						<ul>
+							<li><a href="goalAndObjectives.php">Goal & Objectives</a></li>
+							<li><a href="messageFromChairman.php">Message From Cairman</a></li>
+							<li><a href="massageFrom ManagingDirector.php">Message From Managing Director</a></li>
+						</ul>
+					</li>
+					<li><a href="service.php">Our Services</a></li>
+					<li><a href="findDoctor.php">Doctors</a></li>
+					<li><a href="branch.php">Branches</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="indexx.php">Registration</a></li>
+					<li><a href="appointment.php">Appointment</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class=".messageChairman">
+			<div class="chairmanImg">
+				<img src="img/doctor.png" alt="Pic" width="100%" height="60%" />
+				<h2>Cristina</h2>
+				<h4>Chairmain,XYZ Hospital</h4>
+			</div>
+			<div class="message">
+				<div class="messageHeader">
+					<h2>Message From Chairman</h2>
+					<p>After the liberation war, the health sector of Bangladesh was completely deprived due to various political instability and uncertainity. Unfortunately a significant number of people of this country died on that time only because of incomplete  Unfortunately a significant number of people of this country died on that time only because of incomplete diagnosis of disease and lack of better treatment. When the sufferings were beyond the limit, at that time we established Popular Diagnostic Centre Ltd. in Elephant Road, Dhaka. Our goal was to ensure modern treatment facilities for the mass people of the country.diagnosis of disease and lack of better treatment. When the sufferings were beyond the limit, at that time we established Popular Diagnostic Centre Ltd. in Elephant Road, Dhaka. Our goal was to ensure modern treatment facilities for the mass people of the country. Unfortunately a significant number of people of this country died on that time only because of incomplete diagnosis of disease and lack of better treatment. When the sufferings were beyond the limit, at that time we established Popular Diagnostic Centre Ltd. in Elephant Road, Dhaka. Our goal was to ensure modern treatment facilities for the mass people of the country.
+					XYZ started its journey in June 1983 and within few days due to its accuracy of the reports and quality of the service Popular became an unparallel symbol of reliability and trust from the end of respective doctors and the people of our country.
+
+					To meet the demand of the respective doctors we ensured a modern setup of latest high-tech machineries one after another for complete diagnosis of patients with utmost accuracy. Popular Diagnostic is the first private organization which ensured world latest medical equipments and technology in this country.
+
+					Popular is committed to render the possible standard service to the people of the country at an affordable cost. This will definitely reduce the burden of the government and will make the path of "Health for all" more smooth and effective.</p>
+				</div>
+			</div>
+		</div> 	
+		<footer>
+	        <div class="footer">
+				<h2>Designed by: Adib </h2>
+			</div>
+		</footer>
+	</div> 
+</body>
+</html>
